35ef00e5216bf16075ac538dedd8b52279ca2d92
Mark Allan Paul Sebastian <mapys@cloudstaff.com>
2020-01-28 15:01:25 +0800

    Lesson 112

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -9,7 +9,11 @@ import { LoggingService } from '../logging.service';
   //providers: [LoggingService]
 })
 export class NewAccountComponent {
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {}
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: '+ status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
